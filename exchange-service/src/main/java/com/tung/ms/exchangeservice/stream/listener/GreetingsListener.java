5d2e1edd39287f18ec85ec977d2611200026100c
TungNT66 <tungnt66@fsoft.com.vn>
2019-04-24 11:36:30 +0700

    Support error handler for Kafka when process message error

--- exchange-service/src/main/java/com/tung/ms/exchangeservice/stream/listener/GreetingsListener.java
+++ exchange-service/src/main/java/com/tung/ms/exchangeservice/stream/listener/GreetingsListener.java
@@ -1,6 +1,8 @@
 package com.tung.ms.exchangeservice.stream.listener;
 
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GreetingsListener {
 
+  // TODO support error handling
   @StreamListener(GreetingsStreams.INPUT)
   public void handleGreetings(@Payload Greetings greetings) {
     log.info("Received greetings: {}", greetings);
+    throw new RuntimeException("Process message fail");
+  }
+
+  @ServiceActivator(inputChannel = "greetings.greetingGroup.errors")
+  public void error(Message<?> message) {
+    System.out.println("Handling ERROR: " + message);
   }
 }
